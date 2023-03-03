c2ee1b9a4340dbe05e247cc1f6df9f702dad9db8
JetWig <95491265+JetWig@users.noreply.github.com>
2023-03-03 19:12:27 +0100

    videos + links

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,13 @@
-
+<script>
+  let number;
+  function seconds(check) {
+    number = check.currentTarget.id
+  }
+  let number2;
+  function seconds2(check2) {
+    number2 = check2.currentTarget.id
+  }
+</script>
 <main>
 
 
@@ -22,8 +31,15 @@
       Pour produire de l'hydrogène, on utilise principalement le gaz naturel CH<sub>4</sub>. À la fin de ce processus, on obtient du CO<sub>2</sub> qui contribue à l'effet de serre. Une autre solution est d'utiliser une électrolyse de l'eau. Le principe est que, à l'aide de deux électrodes, l'anode et la cathode, on fait passer du courant électrique dans l'eau, séparant ainsi l'hydrogène de l'oxygène.
     </p>
   </div>
+  <hr class="hr2">
+  <h1 class="subtitle">Qu'avons nous fait?</h1>
+  <p class="subtitle">
+    Nous avons choisi de faire une recherche sur le sujet de l'hydrogène et la mission du robot est de déposer dans l'unité de production de l'hydrogène des matériaux qui serviront à construire une “station service” où les véhicules qui roulent à l'hydrogène peuvent venir faire le plein.
+    Nous avons choisi ce thème car nous entendons beaucoup parler de l'hydrogène dans les médias.
+  </p>
   <hr class="hr1">
   <h2 class="subtitle">Mais encore...</h2>
+
   <div class="grid">
     <div class="card">
       <img src="./Toyota.jpg" alt="Image 1">
@@ -72,8 +88,92 @@
       </div>
     </div>
   </div>
-  <footer>
-    <p>© Copyright 2022 - JetWig & imbwbl</p>
+
+
+
+  <hr class="hr1">
+  <h1 class="subtitle" style="text-decoration: underline">Consultatation d'experts</h1>
+  <h3 class="subtitle">Benjamin Goldmann</h3>
+  <p class="subtitle">
+    Nous sommes allés à l’EPFL interviewer Monsieur Benjamin Goldman, doctorant au laboratoire d’ingénierie moléculaire des nano matériaux optoélectroniques.
+  </p>
+  <div class="videos grid ">
+
+    <div class="card">
+
+      <div class="">
+        <h4>Questions:</h4>
+        <p>- Quelles études avez-vous faits, et faites actuellement? <button id="0" on:click={seconds}>00:00</button></p>
+        <p>- Quel a été votre parcours scolaire? <button id="15" on:click={seconds}>00:15</button></p>
+        <p>- Pourquoi avez-vous choisi le domaine de la chimie? <button id="33" on:click={seconds}>00:33</button></p>
+        <p>- Comment fonctionnent ces feuilles concrétement? <button id="60" on:click={seconds}>01:00</button></p>
+        <p>- Est-ce que vous pensez à l'industrialiser plus tard? <button id="105" on:click={seconds}>01:45</button></p>
+        <p>- Que pourrait être le coût? <button id="143" on:click={seconds}>02:23</button></p>
+        <p>- Quelles entreprises pourraient être visées? <button id="165" on:click={seconds}>02:45</button></p>
+        <p>- Avez-vous posé un brevet? <button id="213" on:click={seconds}>03:33</button></p>
+        <p>- Combien de molécules d'hydrogène sont produites par feuille? <button id="236" on:click={seconds}>03:56</button></p>
+        <p>- Comptez-vous à monter une start-up? <button id="256" on:click={seconds}>04:16</button></p>
+        <p>- Pourquoi utiliser l'hydrogène à la place d'autres énergies? <button id="267" on:click={seconds}>04:27</button></p>
+        <p>- Comment ces molécules se transforment-t-elles? <button id="314" on:click={seconds}>05:14</button></p>
+        <p>- Y-a-t-il des stations à hydrogène en Suisse? <button id="339" on:click={seconds}>05:39</button></p>
+        <p>- Y-a-t-il d'autres laboratoires sur le campus qui travaillent sur l'hydrogène? <button id="367" on:click={seconds}>06:07</button></p>
+        <p>- Comment récuperez-vous l'hydrogène de la feuille? <button id="415" on:click={seconds}>06:55</button></p>
+        <p>- Pourrait-on éviter de mélanger l'oxygène avec l'hydrogène? <button id="439" on:click={seconds}>07:19</button></p>
+        <p>- Quel est l'avenir de l'hydrogène? <button id="478" on:click={seconds}>07:58</button></p>
+        <p>- Comment pourrons nous diminuer le contact avec l'air pendant la transportation? <button id="552" on:click={seconds}>09:12</button></p>
+        <p>- Comment fonctionnerait le moteur à hydrogène? <button id="583" on:click={seconds}>09:43</button></p>
+        <p>- A quelle température l'hydrogène devient-t-il liquide? <button id="620" on:click={seconds}>10:20</button></p>
+        <p>- Quels sont les réserves d'hydrogène sur la planète? <button id="655" on:click={seconds}>10:55</button></p>
+        <p>- Que pensez-vous sur les voitures à hydrogène? <button id="710" on:click={seconds}>11:50</button></p>
+
+      </div>
+    </div>
+    <div class="card">
+      <iframe style="border-radius: 10px" width="600" height="315" src="https://www.youtube.com/embed/uieAoV_ymEA?start={number}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <!-- <iframe style="border-radius: 20px" width="560" height="315" src="https://www.youtube.com/embed/mjVMFTAza3g?start={coucou}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+  </div>
+  <br>
+  <h3 class="subtitle">SoHHytec</h3>
+  <p class="subtitle">
+    Nous sommes également allé interviwer une compagnie s'appelant "SoHHytec" sur le campus de l'EPFL pour en savoir plus sur leur projet.
+  </p>
+  <div class="videos grid ">
+
+    <div class="card">
+
+      <div class="">
+        <h4>Questions:</h4>
+        <p>- Comment fonctionne votre projet? <button id="1" on:click={seconds2}>00:00</button></p>
+        <p>- Que faites vous avec l'énergie produite? <button id="212" on:click={seconds2}>03:33</button></p>
+        <p>- Quelle est la quantité d'énergie produite et consommée?<button id="368" on:click={seconds2}>06:08</button></p>
+      </div>
+    </div>
+    <div class="card">
+      <iframe style="border-radius: 10px" width="600" height="315" src="https://www.youtube.com/embed/moTCngPsY8E?start={number2}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>    </div>
+    <!-- <iframe style="border-radius: 20px" width="560" height="315" src="https://www.youtube.com/embed/mjVMFTAza3g?start={coucou}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> -->
+  </div>
+
+
+  <hr class="hr1">
+  <h2 class="subtitle" style="text-decoration: underline">Quelques liens importants</h2>
+  <div class="card" style="width: 100%; padding: 20px; margin: 0;">
+
+    <div class="">
+      <h4>A step towards solar fuels out of thin air : </h4>
+      <a href="https://youtu.be/2VGMw4dd23Q">https://youtu.be/2VGMw4dd23Q</a>
+      <h4>C'est pas sorcier : Qu'est-ce que l'hydrogène : </h4>
+      <a href="https://youtu.be/WQvDcmRpbN4">https://youtu.be/WQvDcmRpbN4</a>
+      <h4>C'est pas sorcier : Hydrogène, combustible du futur : </h4>
+      <a href="https://youtu.be/7Bn9Gp5kuyI">https://youtu.be/7Bn9Gp5kuyI</a>
+      <h4>C'est pas sorcier : Comment fonctionne une pile à hydrogène : </h4>
+      <a href="https://youtu.be/QDXqF4Qn7Wk">https://youtu.be/QDXqF4Qn7Wk</a>
+      <h4>Esprit sorcier : "Hydrogène, combustible du futur" : </h4>
+      <a style="margin-bottom: 50px" href="https://youtu.be/vyUW9gi0-gg">https://youtu.be/vyUW9gi0-gg</a>
+    </div>
+  </div>
+    <footer>
+    <p>© Copyright 2023 - JetWig & imbwbl</p>
     <i class="fa-brands fa-github"><a href=https://github.com/JetWig>&nbsp -- @JetWig</a></i>
     <br>
     <br>
@@ -82,7 +182,6 @@
 
 
 </main>
-
 
 <style>
 
@@ -106,3 +205,4 @@
     filter: drop-shadow(0 0 2em #646cffaa);
   }
 </style>
+
